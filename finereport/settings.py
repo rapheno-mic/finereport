58280137e209e32e4209386f91aede547682779c
feno <fenocode@gmail.com>
2026-06-28 16:01:55 +0300

    add postgres database

--- finereport/settings.py
+++ finereport/settings.py
@@ -74,10 +74,14 @@ WSGI_APPLICATION = 'finereport.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    "default": {
+		"ENGINE": "django.db.backends.postgresql",
+		"NAME": "postgres",
+		"USER": "postgres",
+		"PASSWORD": "postgres",
+		"HOST": "db", # set in docker-compose.yml
+		"PORT": 5432, # default postgres ports
+	}
 }
 
 
